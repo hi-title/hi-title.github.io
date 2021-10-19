de1f9c019ebeb9413b47b0349d4cb7ffbfc0eba1
hi-title <92512039+hi-title@users.noreply.github.com>
2021-10-19 21:58:04 +0700

    Add files via upload

---
+++ App.svelte 19.svelte
@@ -0,0 +1,29 @@
+<a href="https://youtube.come">Youtube</a>
+<h1>
+	hi my name is Title
+</h1>
+<ol>
+	<li>My story</li>
+</ol>
+<p>
+	my name is title Born on September 6, 2021, currently 16 years old. Studied at Triphat School I like studying academics More than just sitting in an art class But sometimes I like to study art. I like to play many sports such as basketball, sailing, etc.</p>
+
+<h2>
+	 My favorite animal
+</h2>
+<img src="https://www.greencornerlabradors.com/wp-content/uploads/2018/08/19961162_684613971729364_2797900062960265717_n.jpg" alt>
+<img src="https://www.vetbasket.com/wp-content/uploads/2019/04/german-shepherd-405503_1280-e1554367680323.jpg" alt>
+<img src="http://f.ptcdn.info/286/012/000/1384784560-185537741-o.jpg" alt>
+<img src="https://www.nsm.or.th/images/Natural_History_Museum/NEW/2562/Wombat.jpg" alt>
+<img src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZO8slAOzPB63KrSivHmaaHQpp9CMwI.jpg" alt>
+<img src="https://lifestyle.campus-star.com/app/uploads/2019/05/1.png" alt>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiT5Wuii0lGS0ds8-psrmYXdNAb0rYJDqCxJr3-PTdjYI67aOn9-_R4dr4QcsL8Tu4dMA&usqp=CAU" alt>
+<input>
+<ul>
+	<li>this is my picture</li>
+</ul>
+<img src="https://lh3.googleusercontent.com/-NYVTEfgD9yI/YW7DoK2q0jI/AAAAAAAABkI/S4MYTeCsEfo_sckqbh5C9OvqrCm0Qd2UQCEwYBhgLKtQDABHVOhyTuz1dkwT_Y9eBMzWZofIn_6-j-pbVTlwzlHdRBpgaaSUMqIIXmEX_tAeVl6ZuqcIxn3bi8lVFA9JYIgiGAdM2TYRV_YkwLsaYLjQPK3b9Um5BpM9uPGwJaM1o3NpvBHfflThi6SwV6qgNSgPN20Q0ledHxKgZKboF1IgWoQvAYAOJteNfzjhKb-ycoL1aj-DeMKHDu8Fqw_DF4uSak67aoBbe9ljFc_Uquc3kEK-7hNiDR1ijWNDEkwB3wjDb8DBcy8bVgpc0GwB0hb0WgE7lL-_LZ5FfpvwHavi-zJfEPEf3PnN1GLjOGAoEftzO7RqF3-jdzGzKndjSXuZmD0zpjXYvzzEi5AUEZroHrR0Y9031hemEQps4yuVHpPhAVpZFrrJfx2Hk7UF5Xj6qmyFpZcbbqrxZ7C3vDwrhTrTvr2sNFoHdyXM-vLvwGw0WSC6Rv74uGi7W07tnbZdVmHJjq2xW8FUGKfNEhlEraQbCLp7idHJZ8TCh-PVrhzl7-_Zrm1RDQotm9_ffMPYgvkHXHmLHY5DpktEdpLeW6eUoO7QD06ttyB60YSGS9tZRZHrwklnT6JD729zydgO-KOQCwT2G_OXKLLh3HpZN0br-ML61u4sG/w140-h139-p/me%2B%2B26.jpg" alt>
+<img src="https://lh3.googleusercontent.com/-NIKyuYQPI9o/X7Uf5kl0JwI/AAAAAAAABb0/Sx7YWGZqlakks_Cq2c3piwYFUtUcNMopQCEwYBhgLKtQDABHVOhyTuz1dkwT_Y9eBMzWZofIn_6-j-pbVTlwzlHdRBpgaaSUMqIIXmEX_tAeVl6ZuqcIxn3bi8lVFA9JYIgiGAdM2TYRV_YkwLsaYLjQPK3b9Um5BpM9uPGwJaM1o3NpvBHfflThi6SwV6qgNSgPN20Q0ledHxKgZKboF1IgWoQvAYAOJteNfzjhKb-ycoL1aj-DeMKHDu8Fqw_DF4uSak67aoBbe9ljFc_Uquc3kEK-7hNiDR1ijWNDEkwB3wjDb8DBcy8bVgpc0GwB0hb0WgE7lL-_LZ5FfpvwHavi-zJfEPEf3PnN1GLjOGAoEftzO7RqF3-jdzGzKndjSXuZmD0zpjXYvzzEi5AUEZroHrR0Y9031hemEQps4yuVHpPhAVpZFrrJfx2Hk7UF5Xj6qmyFpZcbbqrxZ7C3vDwrhTrTvr2sNFoHdyXM-vLvwGw0WSC6Rv74uGi7W07tnbZdVmHJjq2xW8FUGKfNEhlEraQbCLp7idHJZ8TCh-PVrhzl7-_Zrm1RDQotm9_ffMPYgvkHXHmLHY5DpktEdpLeW6eUoO7QD06ttyB60YSGS9tZRZHrwklnT6JD729zydgO-KOQCwT2G_OXKLLh3HpZN0br-ML61u4sG/w140-h140-p/me.JPG" alt>
+
+
+
