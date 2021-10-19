4d0070f9472d0950537f74aac6ce2d66f3fab9f1
hi-title <92512039+hi-title@users.noreply.github.com>
2021-10-19 22:06:25 +0700

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,35 @@
+<script>
+	let name = 'earth';
+</script>
+<h1>Hi {name}!</h1>
+<h2>
+	start
+</h2>
+<ol>
+	<li>Head1</li>
+	<div>
+		excemple
+	</div>
+	<input>
+</ol>
+<ul>
+	<li>Head2</li>
+	<p>
+		ผมชื่อเติ้ล อยู่ชั้นม.4 เรียนอยู่โรงเรียนไตรพัฒน์ 
+	</p>
+</ul>
+<div>
+	<i>Head3</i>
+</div>
+<a href="https://Youtube.com">Youtube</a>
+<div>
+	<b>Head4</b>
+</div>
+<button>
+	คลิกสิ
+</button>
+<ol>
+	<li>Head5</li>
+</ol>
+<img src="https://lh3.googleusercontent.com/xgAym1zAEToOIV5mzXIdkd4MOx24DmlqHFXI7vAso-Zss_7pkuBpUQM_Eo_hSF0kLjt9BJpQBu3uEpsFlE4=s0" alt="GodZilla">
+	
---
+++ App.svelte ได้แล้ว.svelte
@@ -0,0 +1,26 @@
+
+<h1>
+	hi my name is Title
+</h1>
+<ol>
+	<li>My story</li>
+</ol>
+<p>
+	my name is title Born on September 6, 2021, currently 16 years old. Studied at Triphat School I like studying academics More than just sitting in an art class But sometimes I like to study art. I like to play many sports such as basketball, sailing, etc.</p>
+<ul>
+	<li>นี้คือรูปของผม</li>
+</ul>
+<img src="This PC/Picture/me.jpg" alt="me.jpg">
+<img src="This PC/New Volume(D:)/title>FFDPBR/me 26.jpg" alt="trench">
+<h2>
+	 My favorite animal
+</h2>
+<img src="https://www.greencornerlabradors.com/wp-content/uploads/2018/08/19961162_684613971729364_2797900062960265717_n.jpg" alt>
+<img src="https://www.vetbasket.com/wp-content/uploads/2019/04/german-shepherd-405503_1280-e1554367680323.jpg" alt>
+<img src="http://f.ptcdn.info/286/012/000/1384784560-185537741-o.jpg" alt>
+<img src="https://www.nsm.or.th/images/Natural_History_Museum/NEW/2562/Wombat.jpg" alt>
+<img src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZO8slAOzPB63KrSivHmaaHQpp9CMwI.jpg" alt>
+<img src="https://lifestyle.campus-star.com/app/uploads/2019/05/1.png" alt>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiT5Wuii0lGS0ds8-psrmYXdNAb0rYJDqCxJr3-PTdjYI67aOn9-_R4dr4QcsL8Tu4dMA&usqp=CAU" alt>
+<input>
+<a href="https://classroom.google.com/h" target="blank">classroom</a>
